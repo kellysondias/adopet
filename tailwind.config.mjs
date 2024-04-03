c6e1dcd5492d4b18a4d603c5d350aacdc1bc22db
Kellyson Dias <kellysondias@outlook.com>
2024-04-03 15:51:22 -0300

    🔖feat(tailwind): add responsive classes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      mobile: "360px",
+      tablet: "768px",
+      desktop: "1440px",
+    },
+  },
+  plugins: [],
+};
