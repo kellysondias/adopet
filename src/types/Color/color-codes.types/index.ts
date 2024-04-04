8644ea91d1c353402f9f222f7983e1d62197201d
Kellyson Dias <kellysondias@outlook.com>
2024-04-04 20:18:22 -0300

    🔖feat(types): creats color type

---
+++ src/types/Color/color-codes.types/index.ts
@@ -0,0 +1,5 @@
+type RGB = `#${number}`;
+type HEX = `rgb(${number},${number},${number})`;
+type RGBA = `rgba(${number},${number},${number},${number})`;
+type HSL = `hsl(${number}, ${number}%, ${number}%)`;
+type HSLA = `hsla(${number}, ${number}%, ${number}%, ${number})`;
