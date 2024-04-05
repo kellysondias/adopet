3f56e8dcfaea8228f234e044a25340220795b0e4
Kellyson Dias <kellysondias@outlook.com>
2024-04-05 13:32:58 -0300

    ♻️refactor(types): change color folder name

--- src/types/Color/color-codes.types/index.ts
+++
@@ -1,5 +0,0 @@
-type RGB = `#${number}`;
-type HEX = `rgb(${number},${number},${number})`;
-type RGBA = `rgba(${number},${number},${number},${number})`;
-type HSL = `hsl(${number}, ${number}%, ${number}%)`;
-type HSLA = `hsla(${number}, ${number}%, ${number}%, ${number})`;
---
+++ src/types/color.type/color-codes.types/index.d.ts
@@ -0,0 +1,13 @@
+type RGB = `#${number}`;
+type HEX =
+  | `rgb(${number}, ${number}, ${number})`
+  | `rgb(${number},${number},${number})`;
+type RGBA =
+  | `rgba(${number}, ${number}, ${number}, ${number})`
+  | `rgba(${number},${number},${number},${number})`;
+type HSL =
+  | `hsl(${number}, ${number}%, ${number}%)`
+  | `hsl(${number},${number}%,${number}%)`;
+type HSLA =
+  | `hsla(${number}, ${number}%, ${number}%, ${number})`
+  | `hsla(${number},${number}%,${number}%,${number})`;
