06eb8889346dcea0f10e2ea8dc61d005313edf66
Kellyson Dias <kellysondias@outlook.com>
2024-04-13 17:04:23 -0300

    🔖feat(tailwind) adding new classes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     screens: {
+      small: "300px",
       mobile: "360px",
       tablet: "768px",
       desktop: "1440px",
@@ -11,6 +12,8 @@ export default {
       colors: {
         "primary-color": "#36D6AD",
         "initial-page-color": "#3772FF",
+        "button": "#FC7071",
+        "button-hover": "#FF9D9E"
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
