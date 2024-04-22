fb439b265ce7c46dda3d0d24da37d80fe54b4583
Kellyson Dias <kellysondias@outlook.com>
2024-04-22 16:44:03 -0300

    🔖feat(input sub-component): implementing password visibility state

--- src/components/form-input.component/input.component/index.tsx
+++ src/components/form-input.component/input.component/index.tsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 
 interface Props {
   id: string;
-  type: string;
+  initialType: string;
   placeholder: string;
 }
 
-export const Input: React.FC<Props> = ({ id, type, placeholder }) => {
+export const Input: React.FC<Props> = ({ id, initialType, placeholder }) => {
+  const [type, setType] = useState(initialType)
   const [isActive, setActive] = useState(false);
 
   const isPassword = type === "password";
 
   const togglePasswordVisibility = () => {
-    type = isPassword ? "password" : "text";
+    setType(isPassword ? "text" : "password")
     setActive(!isActive);
   };
 
@@ -25,9 +26,9 @@ export const Input: React.FC<Props> = ({ id, type, placeholder }) => {
         placeholder={placeholder}
         className={`py-2 text-center rounded-md bg-[#F7F7F7] drop-shadow-[0px_2px_2px_rgba(0,0,0,0.25)] w-full`}
       />
-      {isPassword && (
+      {initialType === "password" && (
         <i
-          onClick={() => togglePasswordVisibility}
+          onClick={() => togglePasswordVisibility()}
           className={`fa-regular ${
             isActive ? "fa-eye" : "fa-eye-slash"
           } absolute right-0 self-center mr-3 cursor-pointer text-[#BCBCBC] hover:text-black`}
