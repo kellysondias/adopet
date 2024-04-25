a8c16a966d56e04a16cf716b06afdf97253668ab
Kellyson Dias <kellysondias@outlook.com>
2024-04-25 18:45:46 -0300

    🔖feat(password input): update password input component

--- src/components/form-input.component/input.component/index.tsx
+++ src/components/form-input.component/input.component/index.tsx
@@ -7,14 +7,18 @@ interface Props {
 
 export const PasswordInput: React.FC<Props> = ({ id, placeholder }) => {
   const [type, setType] = useState("password");
-
   const [isActive, setActive] = useState(false);
+  const [value, setValue] = useState("");
+
   const isPassword = type === "password";
 
   const togglePasswordVisibility = () => {
     setType(isPassword ? "text" : "password");
     setActive(!isActive);
   };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setValue(event.target.value);
 
   return (
     <>
@@ -23,15 +27,18 @@ export const PasswordInput: React.FC<Props> = ({ id, placeholder }) => {
         name={type}
         type={type as astroHTML.JSX.HTMLInputTypeAttribute}
         placeholder={placeholder}
+        value={value}
+        onChange={handleInputChange}
         className={`py-2 text-center rounded-md bg-[#F7F7F7] drop-shadow-[0px_2px_2px_rgba(0,0,0,0.25)] w-full`}
       />
-
-      <i
-        onClick={() => togglePasswordVisibility()}
-        className={`fa-regular ${
-          isActive ? "fa-eye" : "fa-eye-slash"
-        } absolute right-0 self-center mr-3 cursor-pointer text-[#BCBCBC] hover:text-black`}
-      />
+      {value !== "" && (
+        <i
+          onClick={() => togglePasswordVisibility()}
+          className={`fa-regular ${
+            isActive ? "fa-eye" : "fa-eye-slash"
+          } absolute right-0 self-center mr-3 cursor-pointer text-[#BCBCBC] hover:text-black`}
+        />
+      )}
     </>
   );
 };
